3449cf8bca1a1d4ec42436e39e63847edcb9c855
chuckberry321 <83531330+chuckberry321@users.noreply.github.com>
2022-01-28 19:12:50 +0300

    changes currency from usd to rub

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1321,7 +1321,7 @@ WIKI_ENABLED = True
 
 COURSE_MODE_DEFAULTS = {
     'bulk_sku': None,
-    'currency': 'usd',
+    'currency': 'rub',
     'description': None,
     'expiration_datetime': None,
     'min_price': 0,
@@ -1913,7 +1913,7 @@ EMBARGO_SITE_REDIRECT_URL = None
 PAYMENT_SUPPORT_EMAIL = 'billing@example.com'
 
 # Setting for PAID_COURSE_REGISTRATION, DOES NOT AFFECT VERIFIED STUDENTS
-PAID_COURSE_REGISTRATION_CURRENCY = ['usd', '$']
+PAID_COURSE_REGISTRATION_CURRENCY = ['rub', '₽']
 
 ################################# EdxNotes config  #########################
 
